9f66cbee1e2d96c4d40cfef12f619d87cebb602c
fang31102001-cyber <fang31102001@gmail.com>
2025-10-26 19:25:18 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,15 @@
-import logging
 from flask import Flask
+from threading import Thread
 
-# Tắt log GET / HTTP/1.1 200 -
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.ERROR)
-
-app = Flask(__name__)
+app = Flask('')
 
 @app.route('/')
 def home():
-    return "✅ AI Futures Bot đang online và chạy ổn định 24/7!"
+    return "Bot AI DeepFlow v2.7 is alive."
 
-def run_server():
-    app.run(host='0.0.0.0', port=10000)
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()

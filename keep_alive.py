def2e2a81e0f9fd6a96e50b680c6b59fa22fb1bc
fang31102001-cyber <fang31102001@gmail.com>
2025-10-26 14:38:29 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,10 +1,15 @@
+import logging
 from flask import Flask
+
+# Tắt log GET / HTTP/1.1 200 -
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "✅ AI Futures Bot đang online!"
+    return "✅ AI Futures Bot đang online và chạy ổn định 24/7!"
 
 def run_server():
     app.run(host='0.0.0.0', port=10000)
